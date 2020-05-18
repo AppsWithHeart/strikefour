84a39e5cb8f979db69b9fef3002aed36155d64f7
Leon <leon@feedmedaily.de>
2020-05-18 20:47:01 +0200

    Wurst add

---
+++ src/strikefour.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const StrikeFour = () => {
+
+    const Square = () => {
+        return (
+            <div>
+                Square WURSTBROT!
+            </div>
+        );
+    }
+
+    return (
+        <>
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        </>
+    );
+}
+
+export default StrikeFour;
